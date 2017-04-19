0e13602cb891083006b0a0b9236dc478a65d31fb
mohitpmathur <mohitpmathur@gmail.com>
2017-04-18 22:29:10 -0400

    Updated conv model to add layers and improve accuracy

--- imdb_sentiment.py
+++ imdb_sentiment.py
@@ -38,18 +38,18 @@ def baseline_model():
     return model
 
 def conv1D_model():
+    top_words = 5000
+    max_words = 500
     # create conv1D model
     model = Sequential()
     model.add(Embedding(top_words, 32, input_length=max_words))
     model.add(Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
-    model.add(MaxPooling1D(pool_size=2))
-    # model.add(Dropout(0.2))
-    # model.add(Conv1D(filters=48, kernel_size=3, padding='same', activation='relu'))
-    # model.add(MaxPooling1D(pool_size=2))
-    # model.add(Conv1D(filters=48, kernel_size=3, padding='same', activation='relu'))
-    # model.add(MaxPooling1D(pool_size=2))
+    model.add(MaxPooling1D(pool_size=max_words))
     model.add(Flatten())
-    model.add(Dense(250, activation='relu'))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dropout(0.2))
     model.add(Dense(1, activation='sigmoid'))
     model.compile(
         loss='binary_crossentropy', 
@@ -79,8 +79,7 @@ if __name__ == "__main__":
 
     # Load only top n words from the dataset
     top_words = 5000
-    (x_train, y_train), (x_test, y_test) = imdb.load_data(
-        num_words=top_words)
+    (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=top_words)
 
     max_words = 500
     x_train = sequence.pad_sequences(x_train, maxlen=max_words)
@@ -90,17 +89,12 @@ if __name__ == "__main__":
     # # fit the model
     # model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=3, batch_size=128, verbose=2)
 
-    '''
-    TO DO:
-        Use cross-validation and compute the accuracy
-    '''
-
     start = time.time()
     model = KerasClassifier(
-        build_fn=LSTM_model, 
+        build_fn=conv1D_model, 
         validation_data=(x_test, y_test),
         epochs=5, 
-        batch_size=128, 
+        batch_size=192, 
         verbose=2)
     model.fit(x_train, y_train)
     # results = cross_val_score(model, x_train, y_train, cv=3)
@@ -113,7 +107,6 @@ if __name__ == "__main__":
         "%H:%M:%S", 
         time.gmtime(elapsed)))
 
-    # ealuate the model
-    # ealuate the model
+    # evaluate the model
     y_pred = model.predict(x_test)
     print ("Accuracy:", metrics.accuracy_score(y_test, y_pred))
